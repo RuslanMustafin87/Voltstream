ed9a1886a090037e6cb7e1ffbdfef26f900222c4
Ruslan Mustafin <ruslanmust87@gmail.com>
2023-02-14 01:05:41 +0500

    section hero

--- src/index.js
+++ src/index.js
@@ -5,5 +5,19 @@ import "../node_modules/bootstrap/js/dist/offcanvas";
 // import "../node_modules/bootstrap/dist/js/bootstrap.min";
 
 import logoImgSrc from "./images/logo.png";
+import houseImgSrc from "./images/house.png";
+import starImgSrc from "./images/star.svg";
+import cartImgSrc from "./images/cart.svg";
+import documentImgSrc from "./images/document.svg";
+// import instagramImgSrc from "./images/instagram.svg";
+// import twitterImgSrc from "./images/twitter.svg";
 
 logoImg.src = logoImgSrc;
+logoImg2.src = logoImgSrc;
+starImg.src = starImgSrc;
+cartImg.src = cartImgSrc;
+documentImg.src = documentImgSrc;
+// instagramImg.src = instagramImgSrc;
+// twitterImg.src = twitterImgSrc;
+houseImg.src = houseImgSrc;
+houseImg2.src = houseImgSrc;
